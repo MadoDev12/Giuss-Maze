2b5c8a09f2cd8a97638387e619f65d561b4026e0
emilyj122 <emilyjoiston@outlook.com>
2024-10-15 08:37:54 +0200

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -5,9 +5,13 @@ import openfl.display.Sprite;
 
 class Main extends Sprite
 {
-	public function new()
-	{
-		super();
-		addChild(new FlxGame(426, 240, MenuState));
-	}
+    public function new()
+    {
+        #if mobile
+        Sys.setCwd(#if android android.content.Context.getExternalFilesDir() + '/' #elseif ios lime.system.System.documentsDirectory #end);
+        #end
+        super();
+        addChild(new FlxGame(426, 240, MenuState));
+        #if android FlxG.android.preventDefaultKeys = [BACK]; #end
+    }
 }
